9d9bf6652b96acc79b2b2bad81624c82be2b9d0a
GabrielRibeiroRS <contato.gabrielsr@gmail.com>
2020-03-11 20:19:15 -0300

    aaaaaaaaaaaaaa

---
+++ app/src/main/java/com/example/projetoaulaagoravai/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.projetoaulaagoravai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+   @Override
+   protected void onStart(){
+        super.onStart();
+   }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
